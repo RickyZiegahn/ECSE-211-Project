88e8ecc84eeac5dd7f23937ba491a38c1fdabfab
Richard Ziegahn <Ricky.ziegahn@gmail.com>
2020-04-01 14:35:57 -0400

    added functions for next engineering team to fill in

--- src/ca/mcgill/ecse211/project/PayloadCollection.java
+++ src/ca/mcgill/ecse211/project/PayloadCollection.java
@@ -2,29 +2,72 @@ package ca.mcgill.ecse211.project;
 
 /**
  * 
- * The payload collection class localizes payloads (among other obstacles)
- * and checks if the detected object is the payload
+ * The payload collection class localizes potential payloads
+ * and checks if the detected object is the payload, and
+ * collects the payload.
  * 
  * @author Ricky
  *
  */
 public class PayloadCollection {
+  
   /**
-   * Localizes potential payloads by sweeping with the ultrasonic sensor
+   * Lines up with the payload, drops the arm, moves under the payload, and lifts it
+   * on top of the robot.
    */
-  void localizePayload() {
+  public void collectPayload() {
     
   }
   
   /**
-   * Collects the payload
+   * Looks for markers to confirm if the potential payload is what the robot
+   * is looking for.
    */
-  void collectPayload() {
+  public boolean confirmPayload() {
+    boolean isPayload = false;
     
+    // function body will have to confirm that the potential payload
+    // by searching for markers that would indicate that it is in fact
+    // the payload
+    
+    return isPayload;
   }
   
   /**
-   * Applies a difference filter to an input array
+   * Uses other functions to sweep a region, and apply filters. Will then process the data to detect
+   * potential payloads.
+   * 
+   * @return array in the form [angle1, distance1, angle2, distance2, ...] showing heading and distance of objects
+   */
+  public double[] getAnglesDistance() {
+    int size = 2*4; // assuming less than 4 objects
+    double[] anglesDistances = new double[size]; //contains 
+    
+    // will need to use sweepDistances, apply the appropriate amount of difference filters (likely two)
+    // and discretize the array. Process should be very similar to getLineAngles from LightLocalizer class.
+    
+    return anglesDistances;
+  }
+  
+  /**
+   * Sweeps the area while measuring distances with the ultrasonic. Turns clockwise
+   * from current heading angle.
+   * 
+   * @param sweepAngle
+   * @return array containing distances read
+   */
+  private float[] sweepDistances(double sweepAngle) {
+    int size = (int) (sweepAngle/2); // will read every two degrees
+    float[] distances = new float[size];
+    
+    // function body must turnBy sweepAngle and while it turns, poll
+    // the ultrasonic distance sensor for distances
+    
+    return distances;
+  }
+  
+  /**
+   * Applies a difference filter to a (non-circular) input array
    * 
    * @param data array of data to apply difference filter to
    * @return filtered data array
